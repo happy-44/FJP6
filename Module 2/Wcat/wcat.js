5205fd0016363495fbc887cdb9de06bac3a056fd
happy-44 <happymittal129@gmail.cvom>
2022-04-24 17:17:27 +0530

    Wcat Project

--- Module 2/Wcat/wcat.js
+++ Module 2/Wcat/wcat.js
@@ -1,7 +1,58 @@
 let fs = require("fs");
 let inputArr = process.argv.slice(2);
-console.log(inputArr);
-let buffer = "";
+// console.log(inputArr);
+let optionsArr = [];
+let fileArr = [];
 for(let i = 0; i < inputArr.length; i++){
-    let fileContent = fs.readFileSync(inputArr[i]);
+    if(inputArr[i].charAt(0) == "-"){
+       optionsArr.push(inputArr[i]);
+    }else{
+        fileArr.push(inputArr[i]);
+    }
 }
+//console.log(optionsArr);
+//console.log(fileArr);
+
+let content = "";
+for(let i = 0; i < fileArr.length; i++){
+    let fileContent = fs.readFileSync(fileArr[i]);
+    content +=  fileContent + "\r\n";
+}
+let buffer = content.split("\r\n");
+//console.log(buffer);
+let isPresent = optionsArr.includes("-s");
+if(isPresent == true){
+    for(let i = 1; i < buffer.length; i++){
+       if(buffer[i] == "" && buffer[i - 1] == ""){
+           buffer[i] = null;
+           //buffer[i - 1] = null;
+       }else if(buffer[i] == "" && buffer[i - 1] == null){
+           buffer[i] = null;
+        }
+    }
+}
+let isNPresent = optionsArr.includes("-n");
+if(isNPresent == true){
+    for(let i = 0; i < buffer.length; i++){
+        buffer[i] = `${i + 1}. ${buffer[i]}`;
+    }
+}
+let counter = 1;
+let isBPresent = optionsArr.includes("-b");
+if(isBPresent == true){
+    for(let i = 0; i < buffer.length; i++){
+        if(buffer[i] != ""){
+            buffer[i] = `${counter}. ${buffer[i]}`;
+            counter++;
+        }
+    }
+}
+console.log(buffer.join("\n"));
+let temp = [];
+for(let i = 0; i < buffer.length; i++){
+    if(buffer[i] != null){
+      temp.push(buffer[i]);
+    }
+}
+buffer = temp;
+//console.log(buffer);
